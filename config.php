8545099b9a275aeecf16f2a98488520ffea32c69
kazuhirosaji <kazuhirosaji@gmail.com>
2014-09-13 22:57:33 -0700

    change site name

--- config.php
+++ config.php
@@ -4,9 +4,9 @@ define('DSN', 'mysql:host=localhost;dbname=dotinstall_poll_php');
 define('DB_USER', 'dbuser');
 define('DB_PASSWORD', 'kazuhirosaji');
 
-define('SITE_URL', 'localhost:3000');
+define('SITE_URL', 'localhost/poll_php');
 
 error_reporting(E_ALL & ~E_NOTICE);
 
-session_set_cookie_params(0, '/tohyo');
+session_set_cookie_params(0, '/poll_php');
 
