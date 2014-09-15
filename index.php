0526178eedd72170f10694e1b2739d4d19865afd
kazuhirosaji <kazuhirosaji@gmail.com>
2014-09-14 21:48:19 -0700

    #05 create poll page

--- index.php
+++ index.php
@@ -10,7 +10,18 @@ require_once('functions.php');
 <head>
 	<meta charset="UTF-8">
 	<title>投票システム</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </head>
 <body>
-	<h1>お料理コンテスト</h1>
+<h1>お料理コンテスト</h1>
+<form action="" method="POST">
+	<img src="photo1.jpg", class="candidate", data-id="1">
+	<img src="photo2.jpg", class="candidate", data-id="2">
+	<img src="photo3.jpg", class="candidate", data-id="3">
+	<img src="photo4.jpg", class="candidate", data-id="4">
+	<p><input type="submit" value="投票する！"></p>
+	<input type="hidden" id="answer" name="answer" value="">
+</form>
+<script>
+</script>
 </body>
